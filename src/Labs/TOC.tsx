f3183cdb4ad22ccebbea439ec9d3a411a71e9198
cristhofer-hernandez <chris75260@gmail.com>
2024-09-25 02:46:09 -0400

    second commit

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <li id="wd-github"><Link to="https://github.com/cristhofer-hernandez/kanbas-react-web-app.git">My Github Repository</Link></li>
         </ul>
     );
 }
